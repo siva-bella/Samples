bb3ba6a9c8bb9d9df02ece02b58bfb766f481b4b
gokila <36073336+siva-bella@users.noreply.github.com>
2019-06-12 15:57:42 +0530

    Update PCOBLPGM.cbl

--- MortgageApplication/cobol/PCOBLPGM.cbl
+++ MortgageApplication/cobol/PCOBLPGM.cbl
@@ -68,7 +68,7 @@
       *    INITIALIZE EMP-REPT-REC.                                     00670033
       *                                                                 00680021
 
-           DISPLAY 'Today is JUNE102019 MONDAY Demo run-02'             00690028
+           DISPLAY 'Today is JUNE122019 WEDNES Demo run-01'             00690028
            OPEN INPUT EMP-FILE                                          00700034
                 OUTPUT EMP-REPT.                                        00710034
            MOVE ZEROES TO EMP-RECS-CNT.                                 00720034
